15ceccb19ea3d0f9b73412625244733a3ec69409
Eippie8735 <chyuchyu600@gmail.com>
2024-06-20 23:53:41 +0900

    spting boot install
    스프링 부트start.spring.io 에서 다운받아서 필요한 디펜던시 넣고 커밋했습니다

---
+++ src/main/java/skyboriii/wicketList/WicketListApplication.java
@@ -0,0 +1,13 @@
+package skyboriii.wicketList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WicketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WicketListApplication.class, args);
+	}
+
+}
